f833a45f08f0c47bc62bb444b15c4f85fd2f1360
adeleong <aderso.deleon@gmail.com>
2016-07-14 11:23:55 -0400

    Finish Controller in MVC

---
+++ ControllerPreview/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerPreview.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public string Index()
+        {
+            return "This is an <br>HTML</br> string";
+        }
+        public string HelloWorld()
+        {
+            return "Hello World string";
+        }
+        public string HelloName(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode( "Hello "+name+". "+ID+ " Times");
+        }
+    }
+}
